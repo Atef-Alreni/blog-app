bab41f0876c3464acf3f30d21f360d0406b252d9
Atef Alreni <atefalreni123456@hotmail.com>
2021-11-02 20:14:13 +0000

    Information Section completed

---
+++ src/components/InformationSection.js
@@ -0,0 +1,28 @@
+import '../styles/InformationSection.css';
+
+function InformationSection() {
+    return (
+        <div className="information-section-container">
+            <div className="free">
+                <div></div>
+                <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor 
+                    invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam 
+                    et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata </p>
+            </div>
+            <div className="global">
+                <div></div>
+                <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy 
+                    eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. 
+                    At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata </p>
+            </div>
+            <div className="valuable">
+                <div></div>
+                <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod 
+                    tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.
+                    At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata </p>
+            </div>
+        </div>
+    )
+}
+
+export default InformationSection
